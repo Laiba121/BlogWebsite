a9b317c122110dbacd02a04ca4103317d3185d3e
Laiba60 <laibasaeed026@gmail.com>
2026-06-10 13:14:40 +0500

    improve code

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import HeroSection from '../components/HeroSection'
+import CategoryGrid from '../components/CategoryGrid'
+import MedicineCard from '../components/MedicineCard'
+import TrendingSection from '../components/TrendingSection'
+import NewsletterSection from '../components/NewsletterSection'
+import Footer from '../components/Footer'   
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <Navbar/>
+        <HeroSection/>
+        <CategoryGrid/> 
+        <MedicineCard/>
+        <TrendingSection/>
+        <NewsletterSection/>
+        <Footer/>   
+    </div>
   )
 }
 
